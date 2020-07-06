63164d975ff6d24e1494dee096ccae3e52ddaedf
Wade Welles <w.welles@multiverse-os.org>
2020-07-06 09:59:56 -0400

    added read functions, only write functions are remaining and then we should have a complete basic functionality; but still will be missing stream read and writes, zero copy which will all be important for a completely useful filesystem abstraction

--- filesystem.go
+++ filesystem.go
@@ -2,16 +2,20 @@ package mumble
 
 import (
 	"fmt"
+	"io"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 )
 
+////////////////////////////////////////////////////////////////////////////////
 // NOTE
 // The goal is to be a very simple filesystem interface, simplifying interaction
 // with the filesystem by abstracting a thin as possible layer making code
 // expressive as possible. To be successful this file must stay small and not
 // complex at all; but also should make working with filesystems very natural,
 // and even have validation for security.
+////////////////////////////////////////////////////////////////////////////////
 
 type Path string
 type File Path
@@ -19,26 +23,60 @@ type Directory Path
 
 func (self Path) String() string { return string(self) }
 
-//func (self Directory) Path() string { return self.path.String() }
-
+////////////////////////////////////////////////////////////////////////////////
 func (self Path) Directory(directory string) Path {
 	return Path(fmt.Sprintf("%s/%s/", self.String(), directory))
 }
 
-func (self Directory) Name() string { return filepath.Base(Path(self).String()) }
-
-func (self File) Name() string {
-	return filepath.Base(Path(self).String())
+func (self Path) File(filename string) Path {
+	return Path(fmt.Sprintf("%s/%s", Path(self).String(), filename))
 }
 
-func (self File) Basename() string {
-	return self.Name()[0:(len(self.Name()) - len(self.Extension()))]
+///////////////////////////////////////////////////////////////////////////////
+// NOTE: Lets always clean before we get to these so no error is possible.
+func (self Path) Info() os.FileInfo {
+	info, _ := os.Stat(self.String())
+	return info
 }
 
-func (self File) Extension() string { return filepath.Ext(Path(self).String()) }
+func (self Path) Permissions() os.FileMode {
+	return self.Info().Mode()
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+func (self Directory) Directory(directory string) Path {
+	return Path(self).Directory(directory)
+}
 
 func (self Directory) File(filename string) Path {
 	return Path(fmt.Sprintf("%s/%s", Path(self).String(), filename))
+}
+
+func (self Directory) String() string { return string(self) }
+func (self Directory) Path() Path     { return Path(self) }
+func (self Directory) Name() string   { return filepath.Base(Path(self).String()) }
+
+///////////////////////////////////////////////////////////////////////////////
+func (self File) String() string   { return string(self) }
+func (self File) Path() Path       { return Path(self) }
+func (self File) Name() string     { return filepath.Base(Path(self).String()) }
+func (self File) Basename() string { return self.Name()[0:(len(self.Name()) - len(self.Extension()))] }
+
+// TODO: In a more complete solution, we would also use magic sequence and mime;
+// but that would have to be segregated into an interdependent submodule or not
+// at all.
+func (self File) Extension() string { return filepath.Ext(Path(self).String()) }
+
+// BASIC OPERATIONS ///////////////////////////////////////////////////////////
+
+// NOTE: In the case of directories, may list contents?
+func (self File) Open() (*os.File, error) {
+	if self.Path().Exists() {
+		return os.Open(self.String())
+	} else {
+		return self.Path().Create(self.Path().Permissions())
+	}
 }
 
 // NOTE: Create directories if they don't exist, or simply create the
@@ -55,12 +93,12 @@ func (self Path) Create(permissions os.FileMode) (*os.File, error) {
 }
 
 func (self Path) Move(path string) error {
-	info, err := os.Stat(path)
-	if err != nil {
+	if info, err := os.Stat(path); err != nil {
 		return err
+	} else {
+		Path(path).Create(info.Mode())
+		return self.Remove()
 	}
-	Path(path).Create(info.Mode())
-	return self.Remove()
 }
 
 func (self Path) Rename(path string) error {
@@ -69,8 +107,23 @@ func (self Path) Rename(path string) error {
 	return os.Rename(self.String(), path)
 }
 
-func (self Path) Remove() error {
-	return os.RemoveAll(self.String())
+func (self Path) Remove() error { return os.RemoveAll(self.String()) }
+
+// IO /////////////////////////////////////////////////////////////////////////
+
+func (self File) Bytes() ([]byte, error) {
+	return ioutil.ReadFile(self.String())
+}
+
+func (self File) HeadBytes(headSize int) ([]byte, error) {
+	var headBytes []byte
+	file, _ := self.Open()
+	readBytes, err := io.ReadAtLeast(file, headBytes, headSize)
+	if readBytes != headSize {
+		return headBytes, fmt.Errorf("error: failed to complete read: read ", readBytes, " out of ", headSize, "bytes")
+	} else {
+		return headBytes, err
+	}
 }
 
 // Validation /////////////////////////////////////////////////////////////////
